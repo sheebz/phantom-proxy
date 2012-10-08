44e2db0fa82698edc910c8f677471a05981955d4
rob schieber <robert.schieber@jpmchase.com>
2012-10-07 20:29:56 -0400

    added args property handling for phantom object added ability to specify port for rpc channel added timeout on waitforSelector function added ability to pass cmd line arguments for phantom via options object

--- test/webpage/open.js
+++ test/webpage/open.js
@@ -4,15 +4,20 @@ var should = require("should"),
 
 describe('#open()', function () {
     it('should return true', function (done) {
-        proxy.page.open('http://www.w3.org', function (result) {
+        this.timeout(0);
+        proxy.page.open('http://www.cnn.com', function (result) {
             assert.equal(result, true);
             done();
         });
     });
     it('should return false when server not working', function (done) {
-        proxy.page.open('http://localhost:32235', function (result) {
-            assert.equal(result, false);
-            done();
+        this.timeout(0);
+        var phantomProxy2 = require('../../index');
+        phantomProxy2.create({ port:1068}, function (proxy2) {
+            proxy2.page.open('http://localhost:32235', function (result) {
+                assert.equal(result, false);
+                done();
+            });
         });
     });
 });
